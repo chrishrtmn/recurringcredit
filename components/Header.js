f2c5a42b2408997ba0ca430e893ad3c51d585123
Chris Hartman <chrishartman@me.com>
2020-05-18 17:35:44 -0400

    Adding initial starter files

---
+++ components/Header.js
@@ -0,0 +1,21 @@
+import Link from 'next/link'
+
+export default function Header() {
+  return (
+    <>
+      <header className='header'>
+        <nav className='nav'>
+          <Link href='/'>
+            <a>Home</a>
+          </Link>
+          <Link href='/blog'>
+            <a>Blog</a>
+          </Link>
+          <Link href='/about'>
+            <a>About</a>
+          </Link>
+        </nav>
+      </header>
+    </>
+  )
+}
