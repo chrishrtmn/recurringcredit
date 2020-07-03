e4cdfc0b1f9a6aee724987d734ab1c4ad1987463
Chris Hartman <chrishartman@me.com>
2020-07-03 11:17:45 -0400

    Adding placeholder Accessibility Statement file

---
+++ pages/accessibility-statement.js
@@ -0,0 +1,35 @@
+// https://www.mecuanywhere.com/accessibility-statement/
+
+import Layout from '../components/layout'
+import Header from '../components/shared/header'
+
+export default function AccessibilityStatement({
+  title,
+  description,
+  ...props
+}) {
+  return (
+    <>
+      <Layout pageTitle={title} description={description}>
+        <Header />
+
+        <main>
+          <div className='wrapper'>
+            <h1>Accessibility Statement</h1>
+          </div>
+        </main>
+      </Layout>
+    </>
+  )
+}
+
+export async function getStaticProps() {
+  const configData = await import(`../siteconfig.json`)
+
+  return {
+    props: {
+      title: configData.default.title,
+      description: configData.default.descriptionAccessibilityStatement,
+    },
+  }
+}
