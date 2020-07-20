33fa3cfda427a02360d4943dd87d6cc7fbc59087
Chris Hartman <chrishartman@me.com>
2020-07-20 12:26:55 -0400

    Adding Hotjar tracking code

--- pages/_document.js
+++ pages/_document.js
@@ -34,7 +34,7 @@ class MyDocument extends Document {
               __html: `
                 (function(h,o,t,j,a,r){
                   h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
-                  h._hjSettings={hjid:1909869,hjsv:6};
+                  h._hjSettings={hjid:${process.env.NEXT_PUBLIC_HOTJAR_TRACKING_ID},hjsv:6};
                   a=o.getElementsByTagName('head')[0];
                   r=o.createElement('script');r.async=1;
                   r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
